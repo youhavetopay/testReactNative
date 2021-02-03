6e0de68edb6875f89bd7b01b25f003627e0f7640
youhavetopay <58547919+youhavetopay@users.noreply.github.com>
2021-02-04 01:05:49 +0900

    이것저것 수정

--- src/App.js
+++ src/App.js
@@ -8,19 +8,11 @@
 
 import React , { Component } from 'react';
 import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  TextInput,
-  StatusBar,
-  Image,
-  TouchableOpacity,
+  SafeAreaView, StyleSheet, ScrollView, View, Text, TextInput,
+  StatusBar, Image, TouchableOpacity, KeyboardAvoidingView, Alert,
 
 } from 'react-native';
 
-import Count from './components/Count'
 
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
@@ -29,7 +21,7 @@ import reducers from './reducers'
 import MyTextInput from './components/MyTextInput'
 import NameInfo from './components/NameInfo'
 import NameInfoList from './components/NameInfoList'
-
+import Count from './components/Count'
 
 
 import {
@@ -42,7 +34,41 @@ import {
 
 class App extends Component {
 
+  constructor(props){
+    super(props)
+    //this.changeInfoValue = this.changeInfoValue.bind(this)
+  }
+
+  answer = {
+    userId:'izone',
+    userPw:'1234'
+  }
+
+  state = {
+    userId:'',
+    userPw:''
+  }
+
+  changeInfoValue = (e, name) =>{
+    
+    this.setState({
+      [name]:e.nativeEvent.text
+    })
+  }
   
+  checkInfoAlert = () =>{
+    Alert.alert(
+      '입력정보 확인',
+      'Id: '+this.state.userId+'\nPw: '+this.state.userPw ,
+      [
+        {
+          text:'확인',
+          onPress: () => console.log('으앙')
+        }
+      ],
+      { cancelable:false}
+    )
+  }
 
   render (){
 
@@ -50,15 +76,31 @@ class App extends Component {
       <>
 
       <SafeAreaView style={styles.backBody}>
-        <View style={{flex:1,  alignItems:'center'}}></View>
-        <View style={{flex:3,  alignItems:'center', padding:10}}>
+        <View style={{flex:3,  alignItems:'center'}}></View>
+        <View style={{flex:13,  alignItems:'center', padding:10}}>
           <Text style={styles.title}>Eyes On Me!</Text>
           <Image style={{width:250, height:100}} source={require('./Image/izone_logo.png')}></Image>
-          <TextInput style={{backgroundColor:'white', borderRadius:20, width:250, height:50, paddingLeft:20,marginTop:30}} placeholder='아이디' ></TextInput>
-          <TextInput style={{backgroundColor:'white', borderRadius:20, width:250, height:50, paddingLeft:20,marginTop:10, marginBottom:20}} placeholder='비밀번호' ></TextInput>
-          <TouchableOpacity style={{backgroundColor:'hotpink', width:250, height:50, borderRadius:20, justifyContent:'center'}}><Text style={{color:'white', textAlign:'center'}}>로그인</Text></TouchableOpacity>
+          
+          <TextInput 
+            style={{backgroundColor:'white', borderRadius:20, width:250, height:50, paddingLeft:20,marginTop:30}} 
+            placeholder='아이디'
+            name = 'userId'
+            onChange={e => this.changeInfoValue(e, 'userId')} ></TextInput>
+
+          <TextInput 
+            style={{backgroundColor:'white', borderRadius:20, width:250, height:50, paddingLeft:20,marginTop:10, marginBottom:20}} 
+            placeholder='비밀번호'
+            name='userPw'
+            onChange={e => this.changeInfoValue(e, 'userPw')} 
+            secureTextEntry={true} ></TextInput>
+          
+          <TouchableOpacity style={{backgroundColor:'hotpink', width:250, height:50, borderRadius:20, justifyContent:'center'}} onPress={this.checkInfoAlert} ><Text style={{color:'white', textAlign:'center'}}>로그인</Text></TouchableOpacity>
         </View>
-        <View style={{flex:1,  alignItems:'center'}}></View>
+
+        <View style={{flex:1,  alignItems:'center'}}>
+          <Text style={{color:'white'}}>Develop by React Native</Text>
+          {/* <Image style={{width:300, height:100}} source={require('./Image/react-native.png')}></Image> */}
+        </View>
       </SafeAreaView>
       
       
